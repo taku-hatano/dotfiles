f4238281abedb2bbf7db14c6a2be95d4098eec2f
hatano <taku_hatano@forcia.com>
2023-11-15 08:56:29 +0900

    update

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,18 +1,2 @@
-require("base")
-require("plugins")
-
-local has = vim.fn.has
-local is_win = has "win32"
-local is_wsl = has "wsl"
-
-if is_win then
-  require("windows")
-end
-if is_wsl then
-  require("wsl")
-end
-
-vim.api.nvim_create_autocmd("BufWritePost", {
-    pattern = { "plugins.lua" },
-    command = "PackerCompile",
-})
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
